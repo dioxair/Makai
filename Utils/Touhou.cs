ec120ac50b96ab351ef45542f27f757f59c90ae4
Samuel Olagunju <me@samola.net>
2024-12-28 18:02:19 -0500

    Fully implement Touhou memory access
    And also fixed my old sigs. My method was completely wrong before..

--- Utils/Touhou.cs
+++ Utils/Touhou.cs
@@ -1,23 +1,87 @@
 ﻿using Memory;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Makai.Utils
 {
     public class Touhou
     {
-        private Mem memory = new();
+        private readonly Mem memory = new();
         private readonly Signatures signatures = new();
         private readonly Offsets offsets = new();
 
-        public float Power;
-        public int Score;
-        public int HighScore;
-        public int Lives;
-        public int SpellCards;
-        public int Graze;
+        private bool autobomb;
+        private bool autocollectItems;
+        private bool invulnerability;
+
+        public Touhou()
+        {
+            memory.OpenProcess("th12.exe");
+        }
+
+        public int Score => GetIntFromSignature(signatures.Score, offsets.Score);
+        public int HighScore => GetIntFromSignature(signatures.HighScore, offsets.HighScore);
+        public int Power => GetIntFromSignature(signatures.Power, offsets.Power);
+        public int Graze => GetIntFromSignature(signatures.Graze, offsets.Graze);
+
+        public float Lives =>
+            GetFloatFromSignature(signatures.Lives, signatures.LivesPart, offsets.Lives, offsets.LivesPart);
+        public float Spellcards =>
+            GetFloatFromSignature(signatures.Spellcards, signatures.SpellcardsPart, offsets.Spellcards,
+                offsets.SpellcardsPart);
+
+        public bool Autobomb
+        {
+            get => autobomb;
+            set
+            {
+                autobomb = value;
+                ModifyFirstByte(signatures.Autobomb, value ? (byte)0xC6 : (byte)0xF6);
+            }
+        }
+        public bool AutocollectItems
+        {
+            get => autocollectItems;
+            set
+            {
+                autocollectItems = value;
+                ModifyFirstByte(signatures.AutocollectItems, value ? (byte)0x00 : (byte)0x0E);
+            }
+        }
+        public bool Invulnerability
+        {
+            get => invulnerability;
+            set
+            {
+                invulnerability = value;
+                ModifyFirstByte(signatures.Invulnerability, value ? (byte)0xC3 : (byte)0x83);
+            }
+        }
+
+        private int GetIntFromSignature(string signature, uint offset)
+        {
+            long codeAddr = memory.AoBScan(signature).Result.FirstOrDefault();
+            string memPointer = (codeAddr + offset).ToString("X");
+            string memAddr = memory.ReadInt(memPointer).ToString("X");
+            int value = memory.ReadInt(memAddr);
+            return value;
+        }
+
+        private float GetFloatFromSignature(string signature1, string signature2, uint offset1, uint offset2)
+        {
+            int p1 = GetIntFromSignature(signature1, offset1);
+            int p2 = GetIntFromSignature(signature2, offset2);
+
+            return float.Parse($"{p1}.{p2}");
+        }
+
+        private void ModifyFirstByte(string signature, byte mod)
+        {
+            long codeAddr = memory.AoBScan(signature).Result.FirstOrDefault();
+            string memAddr = codeAddr.ToString("X");
+            byte[] bytes = memory.ReadBytes(memAddr, 20);
+            bytes[0] = mod;
+
+            memory.WriteBytes(memAddr, bytes);
+        }
     }
 }
--- Utils/TouhouMemory.cs
+++ Utils/TouhouMemory.cs
@@ -1,28 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Makai.Utils
+﻿namespace Makai.Utils
 {
     public class Signatures
     {
-        public string Autobomb => "f6 05 ? ? ? ? ? 0f 84 ? ? ? ? 6a ? 8d 87";
-        public string Invulnerability => "83 ec ? 8b 0d ? ? ? ? c7 86";
-        public string AutocollectItems => "0e 8b 89";
-        public string Score => "e9 ? ? ? ? 01 00 00 01";
-        public string HighScore => "08 f3";
+        public string Autobomb => "? 05 ? ? ? ? ? 0f 84 ? ? ? ? 6a ? 8d 87";
+        public string AutocollectItems => "? 8b 89 2c 0a 00 00 d9 41 08 e9";
+        public string Invulnerability => "? ec ? 8b 0d ? ? ? ? c7 86";
+
+        public string Score => "8b 1d ? ? ? ? 56 8b 35 ? ? ? ? 57";
+        public string HighScore => "a1 ? ? ? ? 8b 15 ? ? ? ? eb";
+        public string Power => "a1 ? ? ? ? 89 54 24 ? 99";
+        public string Graze => "8b 0d ? ? ? ? d9 5c 24 ? d9 ee 8b 54 24 ? d9 5c 24 ? 89 54 24";
+
+        public string Lives => "89 1d ? ? ? ? eb ? a1 ? ? ? ? 3b c5";
+        public string LivesPart => "89 2d ? ? ? ? 75";
+        public string Spellcards => "a1 ? ? ? ? 8b 0d ? ? ? ? 50 51";
+        public string SpellcardsPart => "a1 ? ? ? ? 8b 0d ? ? ? ? 50 51";
     }
 
-    // cheat engine can't disassemble these instructions for some reason so I'll just put the offsets.
     public class Offsets
     {
-        public string Lives => "004B0C98";
-        public string LivesPart => "004B0C9C";
-        public string Spellcards => "004B0CA0";
-        public string SpellcardsPart => "004B0CA4";
-        public string Power => "004B0C48";
-        public string Graze => "004B0CDC";
+        public uint Score => 0x2;
+        public uint HighScore => 0x1;
+        public uint Power => 0x01;
+        public uint Graze => 0x02;
+
+        public uint Lives => 0x02;
+        public uint LivesPart => 0x02;
+        public uint Spellcards => 0x07;
+        public uint SpellcardsPart => 0x01;
     }
 }
