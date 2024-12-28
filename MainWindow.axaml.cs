e2fb07f37e391bf8b2ff2a475563686ad5e54f8b
Samuel Olagunju <me@samola.net>
2024-12-27 23:08:50 -0500

    Add memory library, signatures, and util classes

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,9 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Makai.Utils;
+using Memory;
 
 namespace Makai;
 
 public partial class MainWindow : Window
 {
+    private Touhou touhou = new();
+
     public MainWindow()
     {
         InitializeComponent();
---
+++ Utils/Touhou.cs
@@ -0,0 +1,22 @@
+﻿using Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makai.Utils
+{
+    public class Touhou
+    {
+        private Mem memory = new();
+        private readonly Signatures signatures = new();
+
+        public float Power;
+        public int Score;
+        public int HighScore;
+        public int Lives;
+        public int SpellCards;
+        public int Graze;
+    }
+}
